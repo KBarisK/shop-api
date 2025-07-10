e681a0243e0f81a894829fa5d29c5bbbc58ea05a
Anil <anil0x80@users.noreply.github.com>
2025-07-10 10:28:20 +0300

    test push

--- src/main/java/com/staj/gib/shopapi/ShopApiApplication.java
+++ src/main/java/com/staj/gib/shopapi/ShopApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShopApiApplication {
-
+    // test comment
     public static void main(String[] args) {
         SpringApplication.run(ShopApiApplication.class, args);
     }
