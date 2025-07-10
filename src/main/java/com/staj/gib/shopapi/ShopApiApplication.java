d311f57ad47c82c5ebc506147b15708c9eb6d499
b2200356101 <72985059+b2200356101@users.noreply.github.com>
2025-07-10 11:50:18 +0300

    test commit

--- src/main/java/com/staj/gib/shopapi/ShopApiApplication.java
+++ src/main/java/com/staj/gib/shopapi/ShopApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShopApiApplication {
     // test comment
+    // test 2s
     public static void main(String[] args) {
         SpringApplication.run(ShopApiApplication.class, args);
     }
