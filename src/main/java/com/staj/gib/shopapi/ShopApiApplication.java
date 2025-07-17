a9d4d31bef7a840c8b9cd2e3db3ce4bf72dda4ab
Anil <anil0x80@users.noreply.github.com>
2025-07-17 13:07:25 +0300

    fix application.yaml -> application.yml file extension

--- src/main/java/com/staj/gib/shopapi/ShopApiApplication.java
+++ src/main/java/com/staj/gib/shopapi/ShopApiApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShopApiApplication {
-    // test comment
-    // test 2s
     public static void main(String[] args) {
         SpringApplication.run(ShopApiApplication.class, args);
     }
