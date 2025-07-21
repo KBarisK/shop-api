8f6fa9042261584dabe7416ea02304d4256a2bd5
BARIŞ KILIÇ <bariskilic2003@gmail.com>
2025-07-21 09:34:12 +0300

    cart entity

---
+++ src/main/java/com/staj/gib/shopapi/model/Cart.java
@@ -0,0 +1,42 @@
+package com.staj.gib.shopapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+
+    public enum Status
+    {
+        CLOSED,
+        OPEN
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "cart_id")
+    private UUID cartId;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User userID;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name="status")
+    private Status status;
+}
