7c3e9cd9011dcfc96672f0214436ca36654f502a
Anil <anil0x80@users.noreply.github.com>
2025-07-30 17:37:11 +0300

    fix build error in gradle

--- src/test/java/com/staj/gib/shopapi/ShopApiApplicationTests.java
+++ src/test/java/com/staj/gib/shopapi/ShopApiApplicationTests.java
@@ -1,13 +1,10 @@
 package com.staj.gib.shopapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShopApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
