5c8b0d65290acd0824f07ecaad3860d9913ac0ed
Anil <anil0x80@users.noreply.github.com>
2025-08-11 13:34:24 +0300

    remove unnecessary constructor

--- src/main/java/com/staj/gib/shopapi/constant/RoundingConstants.java
+++ src/main/java/com/staj/gib/shopapi/constant/RoundingConstants.java
@@ -7,9 +7,7 @@ public final class RoundingConstants {
 
     public static final int SCALE = 2;
     public static final RoundingMode ROUNDING = RoundingMode.HALF_UP;
-    
+
     public static final BigDecimal DEFAULT_MONTHLY_INTEREST_RATE = BigDecimal.valueOf(0.0425);
     
-    private RoundingConstants() {
-    }
 }
