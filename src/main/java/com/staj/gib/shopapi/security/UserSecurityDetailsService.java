8c42a2f0a288c33ecb7b9c9bf2e9cd3ce0371e31
BARIŞ KILIÇ <bariskilic2003@gmail.com>
2025-08-14 09:58:57 +0300

    fix circular import

--- src/main/java/com/staj/gib/shopapi/security/UserSecurityDetailsService.java
+++ src/main/java/com/staj/gib/shopapi/security/UserSecurityDetailsService.java
@@ -1,12 +1,9 @@
 package com.staj.gib.shopapi.security;
 
-import com.staj.gib.shopapi.dto.mapper.UserMapper;
-import com.staj.gib.shopapi.entity.User;
-import com.staj.gib.shopapi.enums.ErrorCode;
-import com.staj.gib.shopapi.exception.BusinessException;
-import com.staj.gib.shopapi.repository.UserRepository;
 import com.staj.gib.shopapi.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,8 +12,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserSecurityDetailsService implements UserDetailsService {
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(@Lazy UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -24,10 +25,7 @@ public class UserSecurityDetailsService implements UserDetailsService {
     }
 
     private UserSecurityDetails getUserSecurityDetails(String username) {
-        User user =this.userRepository.findByUsername(username).orElseThrow(()
-                -> new BusinessException(ErrorCode.USER_NOT_FOUND, username));
-
-        return userMapper.userToUserSecurityDetails(user);
+   return this.userService.getUserDetails(username);
     }
 
 }
